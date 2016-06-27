67bf6afde74052020fd35a7dce7aa20ea48fa841
Dylan Ng <ngxinde@gmail.com>
2016-06-27 17:09:02 +0800

    completed work

---
+++ main.js
@@ -0,0 +1,13 @@
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "queen";
+var cardFour = "king";
+
+// if (cardOne === cardFour || cardTwo === cardThree) {
+// 	alert("You found a match!");	
+// }
+// else {
+// 	alert("Sorry, try again.");
+// }
+
+var newGameBoard = document.getElementById('game-board');
